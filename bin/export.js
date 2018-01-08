b68f5c3bf1ab8da14617ab84f643c38dab5586fd
nikvm <hi@nikvm.me>
2018-01-08 19:07:36 +0100

    move logging to cli script

--- bin/export.js
+++ bin/export.js
@@ -1,15 +1,35 @@
 #! /usr/bin/env node
-
+const path = require("path")
 const sapperPirateExport = require("../")
 const startTime = new Date()
+const cwd = process.cwd()
+const pkg = require("../package")
 
-sapperPirateExport()
+const s = num => " ".repeat(Math.max(num, 0))
+const pad = str =>
+  str.length > 33 ? str.substr(0, 33) + "..." + s(4) : str + s(40 - str.length)
+const log =
+  process.env.DEBUG && process.env.DEBUG.includes(pkg.name)
+    ? () => null
+    : console.log.bind(console)
+
+// theres probably easier ways ( i.e. no internet :o )
+console.log(`Exporting like a PIRATE...\n`)
+log(s(4) + "PATH" + s(30) + "=>" + s(4) + "FILE" + "\n")
+
+sapperPirateExport({
+  onPathExported: (pathname, file) => {
+    const l = pathname
+    const r = path.relative(cwd, file)
+    log(pad(l) + " " + pad(r))
+  }
+})
   .then(() => {
     const secondsSince = (new Date() - startTime) / 1000
-    console.log(`Done after ${secondsSince} seconds.`)
+    console.log(`\nDone after ${secondsSince} seconds.`)
     process.exit(0)
   })
   .catch(err => {
-    console.error(err)
+    error(err)
     process.exit(1)
   })
